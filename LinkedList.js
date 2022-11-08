380230fc83007da445d8d01b3d0778b1c9ed52cc
younusaliakash <younusalikash8@gmail.com>
2022-11-08 16:22:48 +0600

    linked list string, array, isEmpty, do empty

--- LinkedList.js
+++ LinkedList.js
@@ -49,6 +49,53 @@ class LinkedList {
         return lastItem;
     }
 
+    //toString
+    toString() {
+        let node = this.head;
+        let text = ""
+        while(node){
+            if(node.next === null){
+                text += `${node.data}`
+                
+            } else{
+                text += `${node.data},`
+            }
+            node = node.next;
+        }
+
+        return text;
+    }
+
+    //to Array
+    toArray (){
+        let node = this.head;
+        let nodes = []
+        while(node){
+            nodes.push(node.data)
+            node = node.next;
+        }
+
+        return nodes;
+
+    }
+
+    //is empty
+    isEmpty(){
+        let  node = this.head;
+
+        if(node === null) {
+            return true
+        } else {
+            return false
+        }
+    }
+
+    //do empty
+    empty(){
+        this.head = null ;
+        return this
+    }
+
 }
 
 class Node {
@@ -66,3 +113,8 @@ console.log(newList.append(new Node (30)))
 console.log(newList.length())
 console.log(newList.firstNode())
 console.log(newList.lastNode())
+console.log(newList.toString())
+console.log(newList.toArray())
+console.log(newList.isEmpty())
+console.log(newList.empty())
+console.log(newList.isEmpty())
